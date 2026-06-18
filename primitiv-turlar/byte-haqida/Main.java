89c38b9318fbb34140b106d778d0b67d8e7c4c7c
masharipov2105 <masharipov2105@gmail.com>
2026-06-18 14:51:32 +0500

    Byte primitiv turini o'rganish, konstantalar qiymatlari bilan tanishuv

--- primitiv-turlar/byte-haqida/Main.java
+++ primitiv-turlar/byte-haqida/Main.java
@@ -1,7 +1,22 @@
+/*
+    -byte primitiv turi javadagi xajmi eng kichik bo'lgan sonlarni ifoda etuvchi tipdir
+    -byte xotira xajmi 1 byte ga teng
+    -byte primitiv turining Byte deb nomlangan wrapper sinfi mavjud
+*/
+
+
+
 public class Main{
 
     public static void main(String[] args){
 
-        System.out.println("Assalomu aleykum!");
+        byte son1 = 12;
+        byte son2 = 15;
+
+        System.out.println("Byte ning maksimal qiymati: " + Byte.MAX_VALUE);
+        System.out.println("Byte ning minimal qiymati: " + Byte.MIN_VALUE);
+        System.out.println("Byte ning xotira xajmi (bitda): " + Byte.SIZE);
+        System.out.println("Byte ning turi: " + Byte.TYPE);
+        System.out.println("Bytening xotira xajmi (baytda): " + Byte.BYTES);
     }
 }
