2b38a03a0f6c6c2f406be63b98ef971b366c6662
masharipov2105 <masharipov2105@gmail.com>
2026-06-19 08:02:20 +0500

    Short primitiv turini o'rganish, Konstanta qiymatlari bilan tanishdik

---
+++ primitiv-turlar/short-haqida/Main.java
@@ -0,0 +1,20 @@
+/*
+    -short primitiv turi javadagi byte dan keyingi eng kichi sonlar bilan ishlaydigan tur xisoblanadi
+    -short xotira xajmi 2 baytga teng,
+    -Short deb nomlanuvchi wrapper sinfiga ega.
+*/
+
+
+
+public class Main{
+
+    public static void main(String[] args){
+
+        //Konstanta qiymatlari
+        System.out.println("Shortning eng kichik qiymati: Short.MIN_VALUE " + Short.MIN_VALUE);
+        System.out.println("Shortning eng katta qiymati: Short.MAX_VALUE " + Short.MAX_VALUE);
+        System.out.println("Short ning primitiv turi nomi: Short.TYPE " + Short.TYPE);
+        System.out.println("Shortning xotira xajmi (bitda): Short.SIZE " + Short.SIZE + " bit");
+        System.out.println("Shortning xotira xajmi (baytda): Short.BYTES " + Short.BYTES + " bayt");
+    }
+}
