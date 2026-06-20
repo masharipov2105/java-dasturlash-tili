9a2d79e8325f8f78452d5b8553b4a985bf3a0419
masharipov2105 <masharipov2105@gmail.com>
2026-06-20 19:57:57 +0500

    Integer tipini o'rganish, asosiy instance metodlar bilan tanishdik

--- primitiv-turlar/int-haqida/Main.java
+++ primitiv-turlar/int-haqida/Main.java
@@ -12,6 +12,7 @@ public class Main{
         int son2 = 1100;
         String son_str = "2255";
         String son_16 = "0x0AA";
+        Integer son_int = Integer.valueOf("77");
 
         //konstanta qiymatlari
         System.out.println("int ning eng kichik qiymati: Integer.MIN_VALUE " + Integer.MIN_VALUE);
@@ -33,5 +34,17 @@ public class Main{
         System.out.println("matn ko'rinishidagi sonni sonni Integer wrapper sinfiga o'raydi va qiymatini qaytaradi: Integer.valueOf(''2255) " + Integer.valueOf(son_str));
         System.out.println("int tipidagi sonni Integer wrapper sinfiga o'raydi va qiymatini qaytaradi: Integer.valueOf(1100) " + Integer.valueOf(son2));
         System.out.println("matn ko'risnishida va sanoq tizimi ham belgilangan xolatda Integer wrapper sinfiga o'raydi sonni 10 lik tizimga o'giradi va qiymatini qaytaradi: Integer.valueOf('2255', 8) " +Integer.valueOf(son_str, 8));
+
+        //instance metodlar
+        System.out.println("Integer obektini qimatini olib uni byte tipiga toarytiradi va qiymatini byte tipida qaytaradi: son_int.byteValue() " + son_int.byteValue());
+        System.out.println("Integer obektini qiymatini boshqa bir int tipidagi o'zgaruvchi qiymati bilan taqqosladi, agar obekt qiymati o'zgaruvchi qiymatidan kichik bo'lsa -1, agar aksincha yani katta bo'lsa 1 yoki teng bo'lsa 0 ni qaytaradi: son_int.compareTo((int) 23) " + son_int.compareTo((int) 78));
+        System.out.println("Integer obektini qiymatini olib uni double tipiga kengaytiradi va qiymatni double tipida qaytaradi: son_int.doubleValue() " + son_int.doubleValue());
+        System.out.println("Integer obektini boshqa bir obekt bilan avval tiplari bo'yicha keyin qiymatlari bo'yiha solishtiradi, agar barchasi teng bo'lsa true qaytaradi aks xolda false qaytaradi: son_int.equals((double) 77) " + son_int.equals((double) 77));
+        System.out.println("Integer obektini qiymatini float tipiga kengaytirib qiymatini float tipida qaytarad: son_int.floatValue() " + son_int.floatValue());
+        System.out.println("Integer obektini qiymatini int tipiga aylantiradi va qiymatini int da qaytaradi: son_int.intValue() " + son_int.intValue());
+        System.out.println("Integer obektini qiymatini long tipiga kengaytriadi va qiymatini long tipida qaytaradi: son_int.longValue() " + son_int.longValue());
+        System.out.println("Integer obektini qiymatini short tipiga toraytiradi va qiymatini short tipida qaytaradi: son_int.shortValue() " + son_int.shortValue());
+        System.out.println("Integer obektini qiymatini String obektiga aylantiradi va qiymatni String tipida qaytaradi: son_int.toString() " + son_int.toString());
+
     }
 }
