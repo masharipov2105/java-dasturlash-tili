58696e370913279e32288001a46a4225e2404f38
masharipov2105 <masharipov2105@gmail.com>
2026-06-21 07:42:17 +0500

    float primitiv tipini o'rganish, static metodlar bilan tanishdik

--- primitiv-turlar/float-haqida/Main.java
+++ primitiv-turlar/float-haqida/Main.java
@@ -13,6 +13,10 @@ public class Main{
 
     public static void main(String[] args){
 
+        float son1 = 10.5f;
+        float son2 = 14.5f;
+        String son_str = "1.23f";
+
         //konstanta qiymatlari
         System.out.println("float tipining xotira xajmi (baytda): Float.BYTES " + Float.BYTES + " bayt");
         System.out.println("float tipining eng katta qiymati: Float.MAX_VALUE " + Float.MAX_VALUE);
@@ -21,5 +25,19 @@ public class Main{
         System.out.println("float tipida ifodalangan musbat cheksizlik : Float.POSITIVE_INFINITY " + Float.POSITIVE_INFINITY);
         System.out.println("float tipining xotira xajmi (bitda): Float.SIZE " + Float.SIZE + " bit");
         System.out.println("float tipining tur nomini qaytaradi: Float.TYPE " + Float.TYPE);
+
+        //static metodlar
+        System.out.println("ikkita float tipidagi sonlarni solishtiradi agar birinchisi ikkinchisidan kichik bo'lsa -1 ni qaytaradi aksincha katta bo'lsa 1 ni qataradi, agar teng bo'lsa 0 ni qaytaradi: Float.compare(10.5f, 14.5f) " + Float.compare(son1, son2));
+        System.out.println("agar son chekli va aniq bo'lsa true qiymatini qataradi, qolgan barcha vayiyatlar uchun flase: Float.isFinite(10.5) " + Float.isFinite(son1));
+        System.out.println("agar son cheksiz b'lsa yani ga teng bo'lmagan kasr sonni 0 ga bo'lganda xosil bo'ladigan cheksizlikni tekshirishda foydalaniladi: Float.isInfinite(0.4f/0) " + Float.isInfinite(0.4f / 0));
+        System.out.println("nolni nolga bo'lish kabi qiymati noaniq natijalarnini tekshirihsda ishlatilinadi: Float.isNan(0.0f / 0) " + Float.isNaN((float)(0.0f / 0)));
+        System.out.println("ikkita float tipidagi o'zgaruvchilar qiymatlari ichidan eng kichigini qaytaradi: Float.min(10.5, 14.5) " + Float.min(son1, son2));
+        System.out.println("Ikkita float tipidagi o'zgaruvchilar qiymatlari ichidan eng kattasini qaytaradi: FLoat.max(10.5, 14.5) " + Float.max(son1, son2));
+        System.out.println("matn ko'rinishidagi qiymatni float primitiv turida qiymatini qaytaradi: Float.parseFloat('1.23f') " + Float.parseFloat(son_str));
+        System.out.println("ikkita float tipidagi qiymatlarni oddiy + operwtori kabi qo'shib natijani qaytaradi: Float.sum(10.5, 14.5) " + Float.sum(son1, son2));
+        System.out.println("float tipidagi qiymatni String obektiga aylantirib qiymatini qaytaradi: Float.toString(14.5f) " + Float.toString(son2));
+        System.out.println("float tipidagi qimatni olib uni Float wrapper sinfiga o'rab obekt yaratadi va qiymatini float da qayatradi: Float.valueOf(10.5f) " + Float.valueOf(son1));
+        System.out.println("Matn ko'rinishida ifodalangan sonni Float wrapper sinfiga o'rab obekt yaratdi va obekt qiymmatini stringda qaytaradi: Float.valueOf('1.23f') " + Float.valueOf(son_str));
+
     }
 }
