64c26467a911a0103cf5e603695f1334dc0e05ed
masharipov2105 <masharipov2105@gmail.com>
2026-06-24 12:06:08 +0500

    Boolean sinfini o'rganish, asosiy konstantalar va ularning qiymatlari bilan tanishdik

---
+++ Sinf-turlari/Boolean-haqida/Main.java
@@ -0,0 +1,20 @@
+/*
+
+    -bool tipi java tilida mantiqiy tur xisoblanib atigi 2 ta qiymatni ifoda eta oladi.
+    -bool tipi nafaqat javada balki barcha datrulash tillarida doimiy qo'llaniladigan tur xisoblanadi
+    -bool tipi ning Boolena deb nomlanuvchi wrapper sinfi mavjud
+
+*/
+
+
+public class Main{
+
+    public static void main(String[] args){
+
+        //konstanta qiymatlari
+        System.out.println("Boolena obekti bo'lib false qiymatiga teng: Boolean.FALSE " + Boolean.FALSE);
+        System.out.println("Boolean obekti bo'lib true qiymatiga teng: Boolean.TRUE " + Boolean.TRUE);
+        System.out.println("Boolena sinfining tipi nomini qaytaradi: Boolean.TYPE " + Boolean.TYPE);
+
+    }
+}
