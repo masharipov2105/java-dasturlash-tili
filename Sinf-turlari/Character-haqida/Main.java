8bd94e1e053a524d2d26900b89a32c1f672368e6
masharipov2105 <masharipov2105@gmail.com>
2026-06-25 14:34:03 +0500

    Character sinfini o'rganish, asosiy konstanta qiymatlari bilan tanishdik

---
+++ Sinf-turlari/Character-haqida/Main.java
@@ -0,0 +1,22 @@
+/*
+    -char tipi nafaqat java tilida balki boshqa ko'plab OOP tillarida mavjud
+    -char uzunligi 1 ga teng String deb tushunish mumkin
+    -String tipidan avval char tipi mavjud bo'lgan va String go'yoki char[] massiviga praporsional
+    -char tipining Character deb nomlanuvchi wrapper sinfi mavjud
+
+*/
+
+
+public class Main{
+
+    public static void main(String[] args){
+
+        //konstanta qiymatlari
+        System.out.println("char tipining xotira xajmi (baytda): Character.BYTES " + Character.BYTES + " bayt");
+        System.out.println("char tipining xotira xajmi (bitda): Character.SIZE " + Character.SIZE + " bit");
+        System.out.println("char tipining eng katta qiymati: Character.MAX_VALUE " + Character.MAX_VALUE);
+        System.out.println("char tipining eng kichik qiymati: Character.MIN_VALUE " + Character.MIN_VALUE);
+        System.out.println("Character sinfining primitiv turi tipini qaytaradi: Character.TYPE " + Character.TYPE);
+
+    }
+}
