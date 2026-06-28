4e2d3c23694f69e4c3999fe8ec2e4d465b7f6607
masharipov2105 <masharipov2105@gmail.com>
2026-06-28 12:38:24 +0500

    Operatorlar bilan ishlash, taqqoslash operatorlari, teng emaslikni tekshirish operatori bilan tanishdik

--- Operatorlar/Taqqoslash-operatorlari/Main.java
+++ Operatorlar/Taqqoslash-operatorlari/Main.java
@@ -21,11 +21,15 @@ public class Main{
         System.out.println("(float 12.1f == double 12.1) " + (son3 == son4));
         System.out.println("('salom' == 'salom') " + (str1 == str2));
         System.out.println("(short 12 == float 12.1f) " + (son1 == son3));
-
+        //System.out.println("(String salom == int 12) " + (str1 == son1)); ikkita tip o'rtasida avtomatik bir biriga kengaytirish imkoni yo'q
         // teng emasligini tekshirish operatori
-        //int s_1 = 27;
-        //int s_2 = 30;
-        //System.out.println("(int 27 != int 30) " + (s_1 != s_2));
+        int s_1 = 27;
+        int s_2 = 30;
+        int s_3 = 30;
+        String str_1 = "salom";
+        System.out.println("(int 27 != int 30) " + (s_1 != s_2));
+        System.out.println("(int 30 != int 30) " + (s_2 != s_3));
+        //System.out.println("(String salom != int 30) " + (str_1 != s_2)); bu ikki tip o'rtasida avtomatik bir biriga kengaytirish imkoni yo'q ERROR
 
     }
 }
