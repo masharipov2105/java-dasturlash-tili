d39ee0d86dc12fa47e10510c21970afc02c3d437
masharipov2105 <masharipov2105@gmail.com>
2026-06-29 22:44:57 +0500

    Operatorlar bilan ishlash, mantiqiy operatorlar, AND (&&) operatori bilan tanishdik

---
+++ Operatorlar/Mantiqiy-operatorlar/Main.java
@@ -0,0 +1,29 @@
+/*
+
+    -mantiqiy operatorlar shart operatotlari bilan birga ishlatilinadi(odatda)
+    -mantiqiy operatotlar bir nechta shart bir vaqtda tekshirilishini taminlaydi
+    -mantiqiy operatorlar boolen tipida qiymat qaytaradi
+
+*/
+
+public class Main{
+
+    public static void main(String[] args){
+
+        // AND (&&) operatori
+
+        boolean qiymat1 = true;
+        boolean qiymat2 = false;
+
+        boolean natija1 = (qiymat2 && qiymat2);
+        boolean natija2 = (qiymat1 && qiymat2);
+        boolean natija3 = (qiymat2 && qiymat1);
+        boolean natija4 = (qiymat1 && qiymat1);
+
+        System.out.println("(bool false && bool false) " + natija1);
+        System.out.println("(bool true && bool false) " + natija2);
+        System.out.println("(bool false && bool true) " + natija3);
+        System.out.println("(bool true && bool true) " + natija4);
+
+    }
+}
