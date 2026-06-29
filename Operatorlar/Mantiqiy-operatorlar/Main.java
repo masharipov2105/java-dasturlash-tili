883ed645f1e4f0f5d82395715ce34d3ac66bb025
masharipov2105 <masharipov2105@gmail.com>
2026-06-29 22:51:06 +0500

    Operatorlar bilan ishlash, mantiqiy operatorlar, OR (||) operatori bilan tanishdik

--- Operatorlar/Mantiqiy-operatorlar/Main.java
+++ Operatorlar/Mantiqiy-operatorlar/Main.java
@@ -25,5 +25,17 @@ public class Main{
         System.out.println("(bool false && bool true) " + natija3);
         System.out.println("(bool true && bool true) " + natija4);
 
+        // OR (||) operatori
+
+        boolean natija5 = (qiymat2 || qiymat2);
+        boolean natija6 = (qiymat1 || qiymat2);
+        boolean natija7 = (qiymat2 || qiymat1);
+        boolean natija8 = (qiymat1 || qiymat1);
+
+        System.out.println("(bool false || bool false) " + natija5);
+        System.out.println("(bool true || bool false) " + natija6);
+        System.out.println("(bool false || bool true) " + natija7);
+        System.out.println("(bool true || bool true) " + natija8);
+
     }
 }
