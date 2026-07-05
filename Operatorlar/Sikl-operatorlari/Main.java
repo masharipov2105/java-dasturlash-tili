55d95ae14d90b556800d55cdd31d6a58776b204c
masharipov2105 <masharipov2105@gmail.com>
2026-07-05 09:26:14 +0500

    Operatorlar bilan ishlash, Sikl-operatorlari, for sikli bilan tanishuv, oddiy o'suvchi sikl yozib ko'rdik

--- Operatorlar/Sikl-operatorlari/Main.java
+++ Operatorlar/Sikl-operatorlari/Main.java
@@ -14,6 +14,8 @@ public class Main{
         int son = 8;
         int xisoblagich = 1;
 
+        //dastlab i o'zgaruvchisi qiymati 0 ga teng, demak dastlab ekranga 0 chiqadi, sikl takrorlanishi asosida i 1 ta ga o'zib boradi
+        // sikl 8 marta davom etishi natijasida esa 0 dan 7 gacha sanaydi, oqibatda yakun 7 bilan tugaydi, yani i 7 gacha o'sadi
         for (int i = 0; i < son; i ++){
 
             System.out.println(xisoblagich + ") " + i);
