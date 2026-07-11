509add43869d6cf9eba1ee875def220c949b8171
masharipov2105 <masharipov2105@gmail.com>
2026-07-11 07:29:25 +0500

    Referens turlar, bir o'lchovli massivlar bilan ishlash, olar massivini yaratdik va for-eich sikli bilan barcha elementlarini chop etib ko'rdik

--- Reference-sinflar/Bir-o'lchovli-massivlar/Main.java
+++ Reference-sinflar/Bir-o'lchovli-massivlar/Main.java
@@ -17,5 +17,31 @@ public class Main{
         // massivning biror elementini chop etish
         System.out.println("massivning 4-indexdagi elementi: " + sonli_massiv[4]);
 
+        //String tipidagi uzunligi oldindan ma'lum bo'lgan bo'sh massiv yaratish
+        String[] oylar_massivi = new String[12];
+
+        // bo'sh massivga qiymatlarni tayinlash
+        oylar_massivi[0] = "yanvar";
+        oylar_massivi[1] = "fevral";
+        oylar_massivi[2] = "mart";
+        oylar_massivi[3] = "aprel";
+        oylar_massivi[4] = "may";
+        oylar_massivi[5] = "iyun";
+        oylar_massivi[6] = "iyul";
+        oylar_massivi[7] = "avgust";
+        oylar_massivi[8] = "sentabr";
+        oylar_massivi[9] = "oktabr";
+        oylar_massivi[10] = "noyabr";
+        oylar_massivi[11] = "dekabr";
+
+        System.out.println("Oylar massivining elementlari");
+        // for sikli yordamida massivning barcha elemeentlarini chop etish
+        // for eich siklidan foydalanib barcha elementlarni chop etish
+        for (String oy : oylar_massivi){
+
+            System.out.print(oy + " ");
+        }
+        // bir probel pastga o'tkazish uchun
+        System.out.println();
     }
 }
