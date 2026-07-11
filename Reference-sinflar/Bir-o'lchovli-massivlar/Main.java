f42e450aba6c815b47201bb2a5c017c944e17ff3
masharipov2105 <masharipov2105@gmail.com>
2026-07-11 07:20:50 +0500

    Referens turlar, bir o'lcovli massivlar, int tipidagi oddiy massivni yaratdik va biror elemetini chop etdik

---
+++ Reference-sinflar/Bir-o'lchovli-massivlar/Main.java
@@ -0,0 +1,21 @@
+/*
+
+    -bir o\lchovli massiv bu ma'lum bir tipga mansub bo'lgan qiymatlarni tartib bilan o'zida saqlovchi va ularni boshqara oladigan bir tip xisoblanadi
+    -java tilida har bir tip asosida bir o'lchovli massiv xosil qilish mumkin
+    -bir o'lchovli massiv dasturlashda juda ko'p foydalaniladigan tur xisoblanadi
+
+*/
+
+
+public class Main{
+
+    public static void main(String[] args){
+
+        // int tipiga mansub massivni yaratish va unga qiymatlarini tayinlash
+        int[] sonli_massiv = {0, 1, 2, 3, 12, -4, 8, 7};
+
+        // massivning biror elementini chop etish
+        System.out.println("massivning 4-indexdagi elementi: " + sonli_massiv[4]);
+
+    }
+}
