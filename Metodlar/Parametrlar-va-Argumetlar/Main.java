6b8d2a6ca28e8ca22f4224e52e7ec47e3f9a4fe9
masharipov2105 <masharipov2105@gmail.com>
2026-07-14 22:52:11 +0500

    Metodlar bilan ishlash, parametrlar va argumetlar, ism va yosh orqali salom beruvchi metod yozib ko'rdik

--- Metodlar/Parametrlar-va-Argumetlar/Main.java
+++ Metodlar/Parametrlar-va-Argumetlar/Main.java
@@ -18,12 +18,26 @@ public class Main{
 
         System.out.printf(a +", " + b + ", "+ c + ", larnig o'rta arifmetigi: %.3f",  arifmetik(a, b, c));
         System.out.println(); // shunchaki yangi qatorga o'tish uchun
+
+        // berilgan ism va yosh asosida salomlashish metodini sinab ko'ramiz
+        String user_ismi = "Anvar";
+        short user_yoshi = 23;
+        salomlashish(user_ismi, user_yoshi); // metodni chaqiramiz
     }
 
+    // 3ta int tipidagi sonlarni o'rta rifmetigini qaytaruvchi metod
     public static double arifmetik(int son1, int son2, int son3){
 
         double orta_arifmetik = (son1 + son2 + son3) / 3.0;
 
         return orta_arifmetik;
+    }
+
+    // ismi va yoshi bo'yicha salom beruvchi metod
+    public static void salomlashish(String ism, short yosh){
+
+        String yakuniy_matn = String.format("Salom %s, yoshi %d", ism, yosh);
+
+        System.out.println(yakuniy_matn);
     }
 }
