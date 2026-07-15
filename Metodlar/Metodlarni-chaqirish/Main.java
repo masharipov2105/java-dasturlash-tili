b16b78aad2f6281536ad6aa8d2d75e03cdac05ce
masharipov2105 <masharipov2105@gmail.com>
2026-07-15 12:15:27 +0500

    metodlar bilan ishlash, metodlarni chaqirish, ikkita son ustida oddiy 4ta arifmetik amal(+,-,*,/) ni bajaradigan 4ta metod yozidk va ularni bir birining ichda chaqirdik

--- Metodlar/Metodlarni-chaqirish/Main.java
+++ Metodlar/Metodlarni-chaqirish/Main.java
@@ -14,10 +14,14 @@ public class Main{
 
         // 3 ta metod yaratamiz va ketma-ketlikda bir birni chaqirib qachon boshlanishi va yakunlanishini kuzatamiz
         // bu dastur bizga LIFO tamoilini mazmunini tushunishga yordam beradi
-
         System.out.println("asosiy main metodi ishga tushdi ...");
         metod1(); // 1-metodni chaqirish
         System.out.println("asosiy main metodi nihoyasiga yetdi");
+
+        // ikkita son ustda oddiy 4ta rifmetik amalni bajaradigan metdlarni yaratib bir birining ichida chaqiramiz
+        int son1 = 16;
+        int son2 = 5;
+        hisoblash(son1, son2);
     }
 
     public static void metod1(){
@@ -38,5 +42,33 @@ public class Main{
 
         System.out.println("metod3 ishga tushdi...");
         System.out.println("metod3 nihoyasiga yetdi");
+    }
+
+    public static void hisoblash(int a, int b){
+
+        int natija = a + b;
+        System.out.println(a + " + " + b + " = " + natija);
+        ayirish(a, b);
+    }
+
+    public static void ayirish(int s1, int s2){
+
+        int natija = s1 - s2;
+        System.out.println(s1 + " - " + s2 + " = " + natija);
+        kopaytirish(s1, s2);
+    }
+
+    public static void kopaytirish(int son_1, int son_2){
+
+        int natija = son_1 * son_2;
+        System.out.println(son_1 + " * " + son_2 + " = " + natija);
+        bolish(son_1, son_2);
+    }
+
+    public static void bolish(int n1, int n2){
+
+        double natija = ((double)n1) / n2;
+        String natija_ = String.format("%.3f", natija);
+        System.out.println(n1 + " / " + n2 + " = " + natija_);
     }
 }
