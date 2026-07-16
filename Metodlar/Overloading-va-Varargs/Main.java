6e57ed8c1e49f1aa49f73a09e13791f852b5abb2
masharipov2105 <masharipov2105@gmail.com>
2026-07-16 09:41:34 +0500

    Metodlar bilan ishlash, Overloading va Varargs tushunchalari, int double va String tipidagi argumetlar asosida 3ta metodni overlading qilib ko'rdik

--- Metodlar/Overloading-va-Varargs/Main.java
+++ Metodlar/Overloading-va-Varargs/Main.java
@@ -24,23 +24,62 @@ public class Main{
         System.out.println(String.format("%d, %d, %d sonlarini (3 ta parametrli metod chaqiriladi) ko'paytirish " + kopaytirish(num_1, num_2, num_3), num_1, num_2, num_3));
         System.out.println(String.format("%d, %d, %d, %d sonlarini (4 ta parametrli metod chaqiriliadi) ko'paytirish: " + kopaytirish(num_1, num_2, num_3, num_4), num_1, num_2, num_3, num_4));
 
-    }
+        // qoshish nomli metodni int, double va String asosida overloading qilishni ko'ramiz
+        int son_1 = 13;
+        int son_2 = 17;
+        double s_1 = 13.1;
+        double s_2 = 16.9;
+        String str_1 = "14";
+        String str_2 = "16";
 
+        // metodlarni chaqirib sinab ko'rish
+        System.out.println(String.format("int %d, int %d qoshish metodini chaqiramiz: " + qoshish(son_1, son_2), son_1, son_2));
+        System.out.println(String.format("double %.3f, double %.3f qoshish metodini chaqiramiz: " + qoshish(s_1, s_2), s_1, s_2));
+        System.out.println(String.format("String %s, String %s, qoshish metodini chaqiramiz: " + qoshish(str_1, str_2), str_1, str_2));
+
+    }
+    // ikki parametrli overloadign qilingan metod
     public static int kopaytirish(int son_1, int son_2){
 
         int natija = son_1 * son_2;
         return natija;
     }
-
+    // uch parametrli overloading qilingan metod
     public static int kopaytirish(int s_1, int s_2, int s_3){
 
         int natija_2 = s_1 * s_2 * s_3;
         return natija_2;
     }
-
+    // to'rt parametrli overloading qilingan metod
     public static int kopaytirish(int n_1, int n_2, int n_3, int n_4){
 
         int natija_3 = n_1 * n_2 * n_3 * n_4;
         return natija_3;
+    }
+
+    // int tipidagi parametrlariga ega overloading qilingan qoshish metodi
+    public static int qoshish(int s_1, int s_2){
+
+        int natija = s_1 + s_2;
+        return natija;
+    }
+
+    // double tipidagi parametrlariga ega overloading qilingan qoshish metodi
+    public static double qoshish(double n_1, double n_2){
+
+        double natija_ = n_1 + n_2;
+        return natija_;
+    }
+
+    // String tipidagi parametrlariga ega overloading qilingan qoshish metodi
+    public static String qoshish(String a_1, String a_2){
+
+        int son_1 = Integer.parseInt(a_1);
+        int son_2 = Integer.parseInt(a_2);
+
+        int natija = son_1 + son_2;
+        String natija_yakuniy = String.valueOf(natija);
+
+        return natija_yakuniy;
     }
 }
