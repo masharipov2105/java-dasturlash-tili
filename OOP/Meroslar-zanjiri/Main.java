630fbfcb1603c2551e06ddd945c85377c5fac620
masharipov2105 <masharipov2105@gmail.com>
2026-07-27 09:00:09 +0500

    OOP bilan ishlash, Meroslar-zanjiri, Ota sinf filedslari va konstruktori shakklantirildi

---
+++ OOP/Meroslar-zanjiri/Main.java
@@ -0,0 +1,37 @@
+/*
+
+    - 2+ meroslar zanjirini xosil qilish mumkin Yani C extends B extends A kabi
+    - faqat konstrultori private bo'lmagan sinflardan meros olish mumkin
+    - ota sinf konstrultorida parametrlar bo'lsa bola sinfda super ni ishlatish majburiy
+    - ota sinf konsturltorida parametr bo'lmasa bola sinfda super yozmasa ham ota ainf konsturktori ishlaydi
+
+*/
+
+public class Main{
+
+    public static void main(String[] args){
+
+    }
+}
+
+// Uch ta sinfdan  iborat meroslar tarmog'ini qurib ko'ramiz
+// Bunda asosiy Ota sinfi undan meros oladigan Bola sinfi va undan meros oladigan Nevara sinfini quramiz
+// Bir biridan meros olishini va ularda o'ziga xos fieldsarni yaratib o'zaro qanday bog'lanishini ko'ramiz
+
+
+class Ota{
+
+    // o'ziga xos va sub classlari uchun umumiy bo'lgan xususiatlar
+    protected String ism;
+    protected int yosh;
+
+    // protected konstruktor shakklantiramiz, qolgan sub classlar meros olishi uchun
+    protected Ota(String ism_, int yosh_){
+
+        // static bo'lmagan fieldslarga dastlabki qiymatlarini tayinlaymiz
+        this.ism = ism_;
+        this.yosh = yosh_;
+        // Ota sinf konstruktori doim chaqirilishini ko'rish uchun konsolga matn chop etmaiz
+        System.out.println("Ota sinf konstruktori initialize bo'ldi!");
+    }
+}
