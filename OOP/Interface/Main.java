38f9688fbd3ee6fc26b44294e6dceb7b394536c1
masharipov2105 <masharipov2105@gmail.com>
2026-08-01 21:34:58 +0500

    OOP bilan ishlash, Interface tushunchasi, samsungPhone nomli yangi class yaratib smartfonMetodlar interfeysini implements qilib barcha abstract metodlarininoverriding qildik

--- OOP/Interface/Main.java
+++ OOP/Interface/Main.java
@@ -25,3 +25,49 @@ interface smartfonMetodlar{
     public abstract void zaryadlash();
     void oyin(); // avtomatik public abstract xolatida bo'ladi
 }
+
+// samsungPhone nomli class yaratib smartfonMetodlar interfeysini implements qilamiz
+class samsungPhone implements smartfonMetodlar{
+
+    // o'ziga xos xususiyatlarni joriy qilamiz
+    protected String model;
+    protected int zaryad;
+
+    // kpnstruktorni yaratamiz
+    protected samsungPhone(String model_, int zaryad_){
+
+        this.model = model_;
+        this.zaryad = zaryad_;
+    }
+
+    // interfeysdan implements qilingan har bir abstract metodini overriding qilishimiz shart
+    @Override
+    public void yoqish(){
+
+        System.out.println(this.model + ": yoqildi...");
+    }
+
+    @Override
+    public void ochir(){
+
+        System.out.println(this.model + ": o'chirildi...");
+    }
+
+    @Override
+    public void zaryadlash(){
+
+        if (this.zaryad == 100){
+
+            System.out.println(this.model + ": zaryad to'liq xolatda " + this.zaryad + "%");
+        }else {
+
+            System.out.println(this.model + ": zaryadlanmoqda ... " + this.zaryad + "%");
+        }
+    }
+
+    @Override
+    public void oyin(){
+
+        System.out.println(this.model + ": o'yin yuklandi, START ---> ");
+    }
+}
