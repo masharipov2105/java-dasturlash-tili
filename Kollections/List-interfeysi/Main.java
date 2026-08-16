9ce470febc83fe14f992ab5e8ca96b10eecccf8b
masharipov2105 <masharipov2105@gmail.com>
2026-08-16 12:44:47 +0500

    Ro'yxatlar tizimi bilan tanishuv, List-interfeysi, Oddiy uzunligi nomalum bo'sh ArrayList ro'yxatini yaratib ko'rdik

---
+++ Kollections/List-interfeysi/Main.java
@@ -0,0 +1,21 @@
+/*
+
+    -Arraylist obektini List sinfining subclasi bo'lib oddiy massiga nisbatan ko'plab qulayliklarga ega ro'yxat tizimi
+    -ArrayList ko'p o'qish amaliyoti bajariladigan tizimlarda o'z samaradorigi bilan ajralib turadi
+    -Arraylist ko'p o'qishdan tashqari oxiriga yozish jarayonida ham o'z tezligi bilan qolgan ro'yxatlardan farqlanadi
+
+
+*/
+
+// kerakli modullarni dasturimiz bilan bog'laymiz
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main{
+
+    public static void main(String[] args){
+
+        //ArrayList yaratib uni imkoniyatlarini ko'rib chiqamiz
+        List<String> array_list = new ArrayList<>(); // Oddiy o'lchami aniq bo'lmagan bosh ro'yxat
+    }
+}
