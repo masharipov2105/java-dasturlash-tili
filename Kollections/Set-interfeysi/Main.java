262eac2c75c35c1901572050ff5b5c0b5225f011
masharipov2105 <masharipov2105@gmail.com>
2026-08-18 10:00:57 +0500

    Ro'yxat tizimlari bilan tanishuv, hash_setning o'ziga xos metodlari, addAll, removeAll va containsAll umulashtirilgan metodlari bilan tnaishik

--- Kollections/Set-interfeysi/Main.java
+++ Kollections/Set-interfeysi/Main.java
@@ -43,5 +43,27 @@ public class Main{
         boolean mavjudmi = hash_set.isEmpty();
 
         System.out.println("hash_set.size() = " + uzunlik + "; hash_set.isEmpty() = " + mavjudmi);
+
+        // HashSetning kengaytirilgan metodlari
+        // addAll, removeAll,containsAll metodlari
+        // ro'yxatlarni kesishmasi, retainAll metodi
+
+        HashSet<String> hash_set2 = new HashSet<>();
+        hash_set2.add("Papka");
+        hash_set2.add("Kitob");
+        hash_set2.add("O'chirg'ich");
+        hash_set2.add("Qalam");
+        // ro'yxatlarni birlashtirish addAll metodi
+        hash_set.addAll(hash_set2);
+        System.out.println("hash_set.add(hash_set2) = " + hash_set);
+
+        //hash_set dan hash_set2 elemetlarni olib tashlash
+        hash_set.removeAll(hash_set2);
+        System.out.println("hash_set.removeAll(hash_set2) = " + hash_set);
+
+        // hash_set ichida hash_set2 da ko'rsatilgan barcha elementlar bormi?
+        boolean natija2 = hash_set.containsAll(hash_set2);
+        System.out.println("hash_set.contaisnAll(hash_set2) = " + natija2);
+
     }
 }
