6b93dcc19276e50170226d765fa9e587ed6bb20b
masharipov2105 <masharipov2105@gmail.com>
2026-08-18 09:27:27 +0500

    Ro'yxat tizimlari bilan tanishuv, Set-interfeysi, ichi bo'sh HashSet ro'yxatini tayinladik

---
+++ Kollections/Set-interfeysi/Main.java
@@ -0,0 +1,22 @@
+/*
+
+    -HashSet Set interfeysida ko'p ishlatiladigan implementlardan biri bo'lib unikallik muhim tizimlarda ajoyib variant
+    -HashSet ma'lumotlarni yig'ish vaqtida dubliklatlarni avtomatik saqlaaydi va unikallikni taminlaydi
+    -HashSet faqat 1 ta null qiymatini saqlay oladi, chunki 2 ta null ham dublikat xisoblanadi
+    -HashSet tizimida tartib kafolatlanmangan, sababi qimatlar hash qiymatlari asosida saqlanadi, qiymatlarni oldindan aytish qiyin
+
+*/
+
+// kerakli modullarni dasturimiz bilan ulash
+import java.util.HashSet;
+
+
+public class Main{
+
+    public static void main(String[] args){
+
+        // HashSet ro'yxatini tayinlash
+        HashSet<String> hash_set = new HashSet<>();
+
+    }
+}
