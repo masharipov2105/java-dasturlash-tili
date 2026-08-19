7772d7e988cd2ab47eb6a10940f0bd37570d5377
masharipov2105 <masharipov2105@gmail.com>
2026-08-19 14:38:24 +0500

    Ro'yxat tizimlari bilan tanishuv, Map-interfeysi, Bo'sh Map ro'yxatini yaratdik

---
+++ Kollections/Map-interfeysi/Main.java
@@ -0,0 +1,19 @@
+/*
+
+    -Map interfeysi java tilida kalit-qiymat jufliklari ro'yxarini boshqarish tizimi
+    -Map interfeysi json ro'yxat tizimi kabi ma'lumotlarni unikal kalit asosida saqlaysi
+    -Map interfeysi asosidagi ro'yxatdagi ma'lumotlarga kalit qiymati asosida murojaat qilinadi
+
+*/
+// kerakli modullarni dasturimizga yuklaymiz
+import java.util.Map;
+import java.util.HashMap;
+
+public class Main{
+	
+	public static void main(String[] args){
+		
+		// Bo'sh HashMap ro'yxatini yaratish
+		Map<String, Integer> hash_map = new HashMap<>();
+	}
+}
