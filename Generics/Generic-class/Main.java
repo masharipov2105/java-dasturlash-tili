6e525ce98567428200a7267f7d474fe71fcab876
masharipov2105 <masharipov2105@gmail.com>
2026-08-21 10:17:55 +0500

    Generic tushunchasi bilan tanishuv, Generic-class, o'zimizni maxsus Xotira nomli kolleksiya tipimizni yaratyapmiz, konstruktorimizni tayinladik

---
+++ Generics/Generic-class/Main.java
@@ -0,0 +1,28 @@
+/*
+
+    -Generic-class bu umuman Kollektionslarni g asosida xisoblanadi
+    -Generic-class yordamida custom kollektion yaratish mumkin
+    -Generic-class Kollektionsda turlar orasidagi farqlarni belgilash uchun kerak. Yani kolleltion bir xil tipdagi qiymatlarni emas turli tipdagi qiymatlarni yig'ib borishi bu esa oxiroda turli xatoliklar keltirib chiqarishi mumkin.
+
+*/
+
+public class Main{
+	
+	public static void main(String[] arga){
+		
+		
+	}
+}
+
+// O'z custom Kollektion sinfimizni quramiz
+// Xotira nomli generic sinfimizni quramiz
+// put get va remove kabi metodlarini tayinlaymiz
+
+class Xotira<T> {
+	//T hali nomalum tbo'lgan turni ofoda etuvchi tip.
+	private T kontent;
+	
+	// konstruktorni tayinlaymiz
+	public Xotira(){} //parametrsiz
+	
+}
