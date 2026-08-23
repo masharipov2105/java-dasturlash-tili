229f272d59780dfb30929d5d01a2fe2701efcc10
masharipov2105 <masharipov2105@gmail.com>
2026-08-23 13:22:03 +0500

    Generic tushunchasi, Generic metodlar, swap nomli T tipidagi arrayni ikkita turli indeksidagi qiymatlarini almashtiradigan generic metod yozdik va uni Integer va String tipidagi arraylar bilan sinab ko'rdik

--- Generics/Generic-metod/Main.java
+++ Generics/Generic-metod/Main.java
@@ -10,14 +10,46 @@ public class Main{
 
     public static void main(String[] args){
 
+        // testing adder generic metod
         System.out.println(adder(2, 3));
         System.out.println(adder("3.14", "Salom"));
+
+        // testing swap generic metod
+        String[] arr1 = {"Salom", "Dunyo", "Yakshanba", "Bugun"}; // create simple array
+
+        // print example array
+        for (String i : arr1){
+
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        swap(arr1, 0, 3); // execute generic metod
+
+        // print example array
+        for (String i : arr1){
+
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 
-    // parametr sifatida berilgan argumetlarni string ko'riinishida qo'shib natijani Stringda qaytaruvchi generic metod yozamiz
+    // parametr sifatida berilgan argumetlarni string ko'riinishida qo'shish
+    //  natijani Stringda qaytaruvchi generic metod yozamiz
     public static <T> String adder(T a, T b){
 
         return (String.valueOf(a) + " " + String.valueOf(b));
+    }
+
+    // T tipidagi arrayni 2ta indeksini qiymatolarni almashtirish
+    // avval arr[0] = x, arr[1] = y , keyin arr[0] = y, arr[1] = x
+
+    public static <T> void swap(T[] array, int indx1, int indx2){
+
+        T helper;
+        helper = array[indx1];
+        array[indx1] = array[indx2];
+        array[indx2] = helper;
     }
 }
 
