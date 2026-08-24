2d50364d644ff15eae679650be556880439ab033
masharipov2105 <masharipov2105@gmail.com>
2026-08-24 10:25:51 +0500

    Asinxron dasturlash tushunchasi, Thread-extends, MyThread nomli sinf yaratib Thread sinfidan meros oldik

---
+++ Threading/Thread-extends/Main.java
@@ -0,0 +1,23 @@
+/*
+
+    -Threading bu asinxorn dasturlash uchun mo'ljallangan java tilidagi standart vosita
+    -Threading siz qurilgan kod odatiy tarzda bir satrdagi kod bajarilib bo'lmagunicha keyingi satrdagi kod bajarilmaydi
+    -Threading asosida qurilgan kod Thread ichidagi kod satri to'liq bajarilishini kutmasdan asosiy Thrrad bajarilishida davom etadi
+
+*/
+
+public class Main{
+	
+	public static void main(String[] args){
+		
+		
+	}
+}
+
+// Thread sinfidan meros olish orqali barcha metodlaridan foydalaniah imkoniga ega bo'lamiz
+//run metodini ovveriding qilish orqali o'zimizni bajarilishi kerak bo'lgan kodni yozamiz
+
+class MyThread extends Thread{
+	
+	
+}
