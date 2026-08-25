24d18cb990656ed322334c72c2ce2a99324513d2
masharipov2105 <masharipov2105@gmail.com>
2026-08-25 15:29:29 +0500

    Asinxron dasturlash tushunchasi, Runnable-interfeysi, Runnable interfeysi yordamida oddiy sikl yaradik, bu namuna uchun, bajarilishga vaqt talab qiladigan dastur

---
+++ Threading/Runnable-interface/Main.java
@@ -0,0 +1,34 @@
+/*
+
+    -Runnable bu interfeys bo'lib ichida bitta run() abstrakt metodiga ega
+    -Runnable interfeysi yordamida alohida kod blokini mudul sifatida alohida obekt sifatida ajrata olamiz
+    -Runnable+Thread zamonaviy va ko'p imkoniyatli kombinatsiya, asinxron dasturlashda
+
+*/
+
+public class Main{
+
+    public static void main(String[] args){
+
+
+        // Runnable interfeysi orqali sikl yaratamiz
+        Runnable task1 = () -> {
+
+            for (int i = 0; i < 5; i ++){
+
+                System.out.println("Runnable jarayoni: " + i);
+
+                // Kutish rejimi Thread bilan
+                try{
+
+                    Thread.sleep(200);
+                } catch (Exception e){
+
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+}
+
+
