c5a174118621bbabaa47fac5a702b5ec9d6e709c
masharipov2105 <masharipov2105@gmail.com>
2026-08-26 21:55:33 +0500

    Asinxron dasturlash tushunchasi, Synchronized, synchronized qo'shildi, race condition tuzatildi, natija: 2000

--- Threading/Synchronized/Main.java
+++ Threading/Synchronized/Main.java
@@ -7,35 +7,50 @@
 */
 
 public class Main {
-    public static int test = 0;
+	
+    public static int test = 0;  // Barcha threadlar uchun umumiy hisoblagich
     
-    public static void main(String[] args) throws Exception {
-        
-        Runnable task1 = () -> {
-            for (int i = 0; i < 1000; i++) {
-                upd();
-            }
-        };
-        
-        Runnable task2 = () -> {
-            for (int i = 0; i < 1000; i++) {
-                upd();
-            }
-        };
-        
-        Thread t1 = new Thread(task1);
-        Thread t2 = new Thread(task2);
-        
-        t1.start();
-        t2.start();
-        
-        t1.join();
-        t2.join();
-        
-        System.out.println("Natija: " + test);  // 2000 dan KICHIK (masalan: 1985)
-    }
-    
-    public static void upd() {
-        test++;  // ⚠️ synchronized YO'Q - race condition bor!
-    }
+	public static void main(String[] args) throws Exception{
+		
+		// 1-vazifa: 1000 marta hisoblagichni oshirish
+		Runnable task1 = () -> {
+			
+			for (int i = 0; i < 1000; i ++){
+			
+			    upd();
+			}
+		};
+		
+		// 2-vazifa: 1000 marta hisoblagichni oshirish
+		Runnable task2 = () -> {
+			
+			for (int i = 0; i < 1000; i ++){
+				
+			    upd();
+			}
+		};
+		
+		// Ikkita thread yaratish
+		Thread t1 = new Thread(task1);
+	    Thread t2 = new Thread(task2);
+	    
+	    // Threadlarni parallel ishga tushirish
+	    t1.start();
+	    t2.start();
+	    
+	    // Ikkala thread ham tugashini kutish (join = "qo'shilish")
+	    t1.join();  // t1 tugaguncha kut
+	    t2.join();  // t2 tugaguncha kut
+	    
+	    // Natijani chop etish (2000 bo'lishi kerak)
+	    System.out.println(test);
+		
+	}
+	
+	// synchronized = bir vaqtda faqat 1 thread ishlatishi mumkin
+	// Bu race condition ni oldini oladi
+	public static synchronized void upd(){
+		
+		test ++;  // test = test + 1 (3 qadam: o'qi, qo'sh, yoz)
+	}
 }
