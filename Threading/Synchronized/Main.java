c8f4f84dfe5849033f011faaf5b3f57baee89e3c
masharipov2105 <masharipov2105@gmail.com>
2026-08-26 21:52:14 +0500

    Asinxron dasturlash tushunchasi, Synchronized, 2 ta thread qo'shildi, lekin synchronized yo'q - natija noto'g'ri

--- Threading/Synchronized/Main.java
+++ Threading/Synchronized/Main.java
@@ -9,11 +9,33 @@
 public class Main {
     public static int test = 0;
     
-    public static void main(String[] args) {
-        // 1000 marta oshiramiz
-        for (int i = 0; i < 1000; i++) {
-            test++;
-        }
-        System.out.println("Natija: " + test);  // 1000
+    public static void main(String[] args) throws Exception {
+        
+        Runnable task1 = () -> {
+            for (int i = 0; i < 1000; i++) {
+                upd();
+            }
+        };
+        
+        Runnable task2 = () -> {
+            for (int i = 0; i < 1000; i++) {
+                upd();
+            }
+        };
+        
+        Thread t1 = new Thread(task1);
+        Thread t2 = new Thread(task2);
+        
+        t1.start();
+        t2.start();
+        
+        t1.join();
+        t2.join();
+        
+        System.out.println("Natija: " + test);  // 2000 dan KICHIK (masalan: 1985)
+    }
+    
+    public static void upd() {
+        test++;  // ⚠️ synchronized YO'Q - race condition bor!
     }
 }
