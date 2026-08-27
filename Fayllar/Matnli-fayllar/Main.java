de448b0083e4bf77b4c0cca8e6a0302bbcbbb4a0
masharipov2105 <masharipov2105@gmail.com>
2026-08-27 20:47:17 +0500

    Fayllar bilan ishlash, Matnli-fayllar, BufferdReader yordamida fayl ichini o'qishni ko'rib chiqdik

--- Fayllar/Matnli-fayllar/Main.java
+++ Fayllar/Matnli-fayllar/Main.java
@@ -1,6 +1,6 @@
 /*
 
-    -Javada fayllar bilan ishlash uchun standart modullariga ega bular File, BufferReder/ BufferWriter
+    -Javada fayllar bilan ishlash uchun standart modullariga ega bular File, BufferedReader/ BufferedWriter
     -File moduli yordamida faylni obekt sifatida qarashimiz mumkin bo'ladi
     -BufferReder yordamida fayl obektini tez va qulay o'qish imkoniyatiga ega bo'lamiz
     -BufferWriter yordamida fayl obketiga ma'lumot yozishimiz mumkin
@@ -8,8 +8,9 @@
 */
 // kerakli modullarni dasturimizga bog'laymiz
 import java.io.File;
-
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
 public class Main{
 
     public static void main(String[] args){
@@ -23,5 +24,18 @@ public class Main{
         System.out.println("fayl yo'li: " + fayl.getPath());
         System.out.println("absolut yo'li: " + fayl.getAbsolutePath());
         System.out.println("fayl o'lchami: " + fayl.length() + " bayt");
+
+        // BufferedReader yordamida faylni o'qish
+
+        try{
+            // BufferedReader FileReader obketini qabul qiladi
+            BufferedReader br = new BufferedReader(new FileReader(fayl));
+
+            String line; // yordamich, har bir iteratsiyada faylning har bir satriga teng bo'ladi
+            while ((line = br.readLine()) != null){
+
+                System.out.println(line); // satrlarni chop etish
+            }
+        } catch(IOException e){ e.printStackTrace();}
     }
 }
